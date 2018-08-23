2a5f0307d46c47ed89fa01ac47e71e094957b826
Razmik Tovmasyan <razmikt@internal.synopsys.com>
2018-08-23 11:23:05 +0400

    Add InitializeDisplay method
    Minor changes: Now convas is square

--- GameOfLife/MainWindow.xaml.cs
+++ GameOfLife/MainWindow.xaml.cs
@@ -20,9 +20,46 @@ namespace GameOfLife
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int ROWS_COLS = 30;
+
+        Rectangle[,] fields = new Rectangle[ROWS_COLS, ROWS_COLS];
+
         public MainWindow()
         {
             InitializeComponent();
+
+            InitializeDisplay();
+        }
+
+        private void InitializeDisplay()
+        {
+            displayCanvas.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+            displayCanvas.Arrange(new Rect(0.0, 0.0, displayCanvas.DesiredSize.Width, displayCanvas.DesiredSize.Height));
+
+            for (int i = 0; i < ROWS_COLS; i++)
+            {
+                for (int j = 0; j < ROWS_COLS; j++)
+                {
+                    Rectangle ractangle = new Rectangle();
+
+                    ractangle.Height = displayCanvas.ActualHeight / ROWS_COLS - 2.0;
+                    ractangle.Width = displayCanvas.ActualWidth / ROWS_COLS - 2.0;
+
+                    ractangle.Fill = Brushes.LightGray;
+                    displayCanvas.Children.Add(ractangle);
+                    Canvas.SetTop(ractangle, i * displayCanvas.ActualHeight / ROWS_COLS);
+                    Canvas.SetLeft(ractangle, j * displayCanvas.ActualWidth / ROWS_COLS);
+
+                    ractangle.MouseDown += Ractangle_MouseDown;
+
+                    fields[i, j] = ractangle;
+                }
+            }
+        }
+
+        private void Ractangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
         }
 
         private void buttonStartStop_Click(object sender, RoutedEventArgs e)
